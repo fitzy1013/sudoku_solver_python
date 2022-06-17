715e5c0553331a9c1f1b578cd8e74e517f5e7678
fitzy1013 <53210026+fitzy1013@users.noreply.github.com>
2022-06-17 15:56:44 +1000

    Added the Fill Matrix Method which at this stage can set up games with 10 and 15 numbers filled and solve on a regular basis

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random
+
+
 def create_matrix():
     matrix_inner = []
     matrix = []
@@ -14,39 +17,22 @@ def create_matrix():
     return matrix
 
 
-def valid_change(matrix: list, row: int, col: int, value: int, user: bool):
-    for i in range(0, 9):
-        if matrix[i][col - 1] == value:
-            if user:
-                print("Value Already being used in that Collum")
-            return False
-        elif matrix[row - 1][i] == value:
-            if user:
-                print("Value Already being used in that Row")
-            return False
-
-    if row < 4:
-        temp_row = 0
-    elif row < 7:
-        temp_row = 3
-    else:
-        temp_row = 6
-
-    if col < 4:
-        temp_col = 0
-    elif col < 7:
-        temp_col = 3
-    else:
-        temp_col = 6
-
-    for i in range(0, 3):
-        for j in range(0, 3):
-            if matrix[temp_row + i][temp_col + j] == value:
-                if user:
-                    print("Value Already being used in that Section")
-                return False
-
-    return True
+def fill_matrix(matrix: list):
+    cells_filled = 0
+    number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    while cells_filled < 15:
+        temp_number_list = number_list
+        row = random.randint(0, 8)
+        col = random.randint(0, 8)
+        if matrix[row][col] == -1:
+            while len(temp_number_list) != 0:
+                value = random.randint(0, len(temp_number_list) - 1)
+                if isValid(matrix, row, col, temp_number_list[value]):
+                    matrix[row][col] = temp_number_list[value]
+                    cells_filled += 1
+                    break
+                else:
+                    del temp_number_list[value]
 
 
 def print_matrix(matrix: list):
@@ -63,17 +49,16 @@ def print_matrix(matrix: list):
     print("\n-------------------------------------------")
 
 
-def input_value(matrix: list, user: bool):
+def input_value(matrix: list):
     while True:
         col_input = int(input("Input Collum Cell you would like to change 1 to 9: "))
         row_input = int(input("Input Row Cell you would like to change from 1 to 9: "))
         number_input = int(input("Input Number you want to place inside from 1 to 9: "))
         if 0 < row_input < 10 and 0 < col_input < 10 and 0 < number_input < 10 \
-                and valid_change(matrix, row_input, col_input, number_input, True):
+                and isValid(matrix, row_input, col_input, number_input):
             break
         else:
-            if user:
-                print("Invalid Input Please Try Again")
+            print("Invalid Input Please Try Again")
     matrix[row_input - 1][col_input - 1] = number_input
 
     return matrix
@@ -124,6 +109,8 @@ def solve_matrix(matrix: list, i=0, j=0):
 
 def main():
     matrix = create_matrix()
+    fill_matrix(matrix)
+    print_matrix(matrix)
     solve_matrix(matrix)
     print_matrix(matrix)
 
